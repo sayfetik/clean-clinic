991ff795cafe56d6b042714bdd5cb371c7f2bd71
Adilia Saifetdirova <sayfetik@Adilias-MacBook-Pro.local>
2025-02-12 10:12:08 +0300

    Add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,49 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// eslint.config.js
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginTypescript from '@typescript-eslint/eslint-plugin';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import typescriptParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: typescriptParser, // Используем парсер для TypeScript
+      parserOptions: {
+        project: './tsconfig.app.json', // Путь к вашему tsconfig.json
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      import: eslintPluginImport,
+      'jsx-a11y': eslintPluginJsxA11y,
+      '@typescript-eslint': eslintPluginTypescript,
+      prettier: eslintPluginPrettier,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc', caseInsensitive: false },
+          distinctGroup: true,
+          named: false,
+          warnOnUnassignedImports: false,
+        },
+      ],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/triple-slash-reference': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/consistent-type-assertions': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      curly: ['error', 'all'],
+      'no-irregular-whitespace': ['error', { skipTemplates: true, skipStrings: true }],
+      'no-console': ['error', { allow: ['info', 'error', 'warn'] }],
     },
-  }
-)
+  },
+];
