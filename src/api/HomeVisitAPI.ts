7e8b4c18024bccaa0b912a0dd49688b766a6df87
Adilia Saifetdirova <sayfetik@Adilias-MacBook-Pro.local>
2025-05-09 04:06:47 +0300

    Add homeVist admin (finished)

--- src/api/HomeVisitAPI.ts
+++ src/api/HomeVisitAPI.ts
@@ -1,3 +1,4 @@
+import { HomeVisitServiceType } from '../lib/types'
 import { get, post } from './methods'
 
 const section = import.meta.env.VITE_HOME_VISIT as string
@@ -11,4 +12,52 @@ export const getHomeVisitPage = async () => {
     await createHomeVisit()
     return await get(`${section}/GetHomeVisit`)
   }
+}
+
+export const editHomeVisit = async (formData: FormData) => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/${section}/EditHomeVisit`, {
+    method: 'PUT',
+    body: formData,
+  })
+  if (!res.ok) {
+    throw new Error('Ошибка при обновлении HomeVisit')
+  }
+  return await res.json()
+}
+
+export const editHomeVisitService = async (id: number, formData: FormData) => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/${section}/EditHomeVisitSubcription${id}`, {
+    method: 'PUT',
+    body: formData,
+  })
+  if (!res.ok) {
+    throw new Error('Ошибка при обновлении услуги HomeVisit')
+  }
+  return await res.json()
+}
+
+export const deleteHomeVisitService = async (id: number) => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/${section}/DeleteHomeVistService/${id}`, {
+    method: 'DELETE',
+  })
+  if (!res.ok) {
+    throw new Error('Ошибка при удалении услуги HomeVisit')
+  }
+}
+
+export const createHomeVisitService = async (item: HomeVisitServiceType) => {
+  const formData = new FormData()
+  formData.append('Name', item.name)
+  formData.append('Cost', String(item.cost))
+  if (item.img instanceof File) {
+    formData.append('Image', item.img)
+  }
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/${section}/CreateHomeVisitService`, {
+    method: 'POST',
+    body: formData,
+  })
+  if (!res.ok) {
+    throw new Error('Ошибка при создании услуги HomeVisit')
+  }
+  return await res.json()
 }
